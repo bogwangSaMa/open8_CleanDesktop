6f08d9f510eb7f6132a7eed8466afcbd12c1055b
kimjeongho12 <164467350+kimjeongho12@users.noreply.github.com>
2024-06-13 15:28:19 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿using System.Reflection.Emit;
+
+namespace VolumeControl
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private TrackBar trackBarVolume;
+        private Label labelVolume;
+        private TrackBar trackBarMicVolume;
+        private Label labelMicVolume;
+        private TrackBar[] trackBars;
+        private Label[] labels;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.labelVolume = new System.Windows.Forms.Label();
+            this.trackBarMicVolume = new System.Windows.Forms.TrackBar();
+            this.labelMicVolume = new System.Windows.Forms.Label();
+            this.trackBars = new System.Windows.Forms.TrackBar[10];
+            this.labels = new System.Windows.Forms.Label[10];
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMicVolume)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.Location = new System.Drawing.Point(12, 12);
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(260, 45);
+            this.trackBarVolume.TabIndex = 0;
+
+            // 
+            // labelVolume
+            // 
+            this.labelVolume.AutoSize = true;
+            this.labelVolume.Location = new System.Drawing.Point(12, 60);
+            this.labelVolume.Name = "labelVolume";
+            this.labelVolume.Size = new System.Drawing.Size(49, 13);
+            this.labelVolume.TabIndex = 1;
+            this.labelVolume.Text = "Volume: ";
+
+            // 
+            // trackBarMicVolume
+            // 
+            this.trackBarMicVolume.Location = new System.Drawing.Point(12, 90);
+            this.trackBarMicVolume.Name = "trackBarMicVolume";
+            this.trackBarMicVolume.Size = new System.Drawing.Size(260, 45);
+            this.trackBarMicVolume.TabIndex = 2;
+
+            // 
+            // labelMicVolume
+            // 
+            this.labelMicVolume.AutoSize = true;
+            this.labelMicVolume.Location = new System.Drawing.Point(12, 140);
+            this.labelMicVolume.Name = "labelMicVolume";
+            this.labelMicVolume.Size = new System.Drawing.Size(66, 13);
+            this.labelMicVolume.TabIndex = 3;
+            this.labelMicVolume.Text = "Mic Volume: ";
+
+            // 이퀄라이저 트랙바 및 레이블 초기화
+            string[] freqLabels = { "32Hz", "64Hz", "125Hz", "250Hz", "500Hz", "1kHz", "2kHz", "4kHz", "8kHz", "16kHz" };
+            for (int i = 0; i < 10; i++)
+            {
+                this.trackBars[i] = new TrackBar();
+                this.labels[i] = new Label();
+
+                this.trackBars[i].Location = new System.Drawing.Point(12 + i * 30, 170);
+                this.trackBars[i].Name = "trackBar" + i;
+                this.trackBars[i].Orientation = Orientation.Vertical;
+                this.trackBars[i].Size = new System.Drawing.Size(45, 150);
+                this.trackBars[i].TabIndex = 4 + i;
+                this.trackBars[i].Minimum = -10;
+                this.trackBars[i].Maximum = 10;
+                this.trackBars[i].Value = 0;
+
+                this.labels[i].Location = new System.Drawing.Point(12 + i * 30, 320);
+                this.labels[i].Name = "label" + i;
+                this.labels[i].Size = new System.Drawing.Size(45, 13);
+                this.labels[i].TabIndex = 14 + i;
+                this.labels[i].Text = freqLabels[i];
+
+                this.Controls.Add(this.trackBars[i]);
+                this.Controls.Add(this.labels[i]);
+            }
+
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 350);
+            this.Controls.Add(this.labelMicVolume);
+            this.Controls.Add(this.trackBarMicVolume);
+            this.Controls.Add(this.labelVolume);
+            this.Controls.Add(this.trackBarVolume);
+            this.Name = "Form1";
+            this.Text = "Volume Control with Equalizer";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarMicVolume)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using CoreAudioApi;
+using NAudio.Dsp;
+
+namespace VolumeControl
+{
+    public partial class Form1 : Form
+    {
+        private MMDeviceEnumerator devEnum;
+        private MMDevice renderDevice;
+        private MMDevice captureDevice;
+        private BiQuadFilter[] filters;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeVolumeControl();
+            InitializeMicVolumeControl();
+            InitializeEqualizer();
+        }
+
+        private void InitializeVolumeControl()
+        {
+            devEnum = new MMDeviceEnumerator();
+            renderDevice = devEnum.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+
+            // 트랙바 초기 설정
+            trackBarVolume.Minimum = 0;
+            trackBarVolume.Maximum = 100;
+            trackBarVolume.Value = (int)(renderDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            labelVolume.Text = $"Volume: {trackBarVolume.Value}%";
+
+            // 트랙바 이벤트 핸들러 연결
+            trackBarVolume.Scroll += TrackBarVolume_Scroll;
+        }
+
+        private void TrackBarVolume_Scroll(object sender, EventArgs e)
+        {
+            // 볼륨 설정
+            float volume = trackBarVolume.Value / 100.0f;
+            renderDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+            labelVolume.Text = $"Volume: {trackBarVolume.Value}%";
+        }
+
+        private void InitializeMicVolumeControl()
+        {
+            captureDevice = devEnum.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eMultimedia);
+
+            // 마이크 볼륨 트랙바 초기 설정
+            trackBarMicVolume.Minimum = 0;
+            trackBarMicVolume.Maximum = 100;
+            trackBarMicVolume.Value = (int)(captureDevice.AudioEndpointVolume.MasterVolumeLevelScalar * 100);
+            labelMicVolume.Text = $"Mic Volume: {trackBarMicVolume.Value}%";
+
+            // 트랙바 이벤트 핸들러 연결
+            trackBarMicVolume.Scroll += TrackBarMicVolume_Scroll;
+        }
+
+        private void TrackBarMicVolume_Scroll(object sender, EventArgs e)
+        {
+            // 마이크 볼륨 설정
+            float volume = trackBarMicVolume.Value / 100.0f;
+            captureDevice.AudioEndpointVolume.MasterVolumeLevelScalar = volume;
+            labelMicVolume.Text = $"Mic Volume: {trackBarMicVolume.Value}%";
+        }
+
+        private void InitializeEqualizer()
+        {
+            // 이퀄라이저 필터 초기화 (각 주파수 대역별로)
+            filters = new BiQuadFilter[10];
+            float[] frequencies = { 32, 64, 125, 250, 500, 1000, 2000, 4000, 8000, 16000 };
+
+            for (int i = 0; i < filters.Length; i++)
+            {
+                filters[i] = BiQuadFilter.PeakingEQ(44100, frequencies[i], 1.0f, 0);
+                trackBars[i].Scroll += (s, e) => UpdateEqualizer(i, trackBars[i].Value);
+            }
+        }
+
+        private void UpdateEqualizer(int band, int gain)
+        {
+            float gainFactor = gain / 10.0f;
+            filters[band].Gain = gainFactor;
+        }
+    }
+}
